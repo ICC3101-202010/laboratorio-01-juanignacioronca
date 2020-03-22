5baa69e11b05fe131e12c6f9c31bbdc49918c6dd
juan ignacio roncagliolo <jironcagliolo@miuandes.cl>
2020-03-22 18:59:38 -0300

    Cracion Clase Persona

--- Laboratorio 1 Juan Roncagliolo/Laboratorio 1 Juan Roncagliolo/Clase Persona.cs
+++ Laboratorio 1 Juan Roncagliolo/Laboratorio 1 Juan Roncagliolo/Clase Persona.cs
@@ -8,5 +8,16 @@ namespace Laboratorio_1_Juan_Roncagliolo
 {
     class Clase_Persona
     {
+        //Creacion de atributos
+        public string Name;
+        public string LastName;
+        //Creacion metodo lanzar
+        public int Lanzar()
+        {
+            Random rnd = new Random();
+            int hand = rnd.Next(0,3);
+            return hand;
+        }
+
     }
 }
