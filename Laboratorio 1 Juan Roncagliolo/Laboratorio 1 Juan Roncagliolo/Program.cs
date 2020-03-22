489340ee450558a79af5caacfa3a91daf6303439
juan ignacio roncagliolo <jironcagliolo@miuandes.cl>
2020-03-22 19:28:52 -0300

    Parte 1 Completa

--- Laboratorio 1 Juan Roncagliolo/Laboratorio 1 Juan Roncagliolo/Program.cs
+++ Laboratorio 1 Juan Roncagliolo/Laboratorio 1 Juan Roncagliolo/Program.cs
@@ -10,7 +10,36 @@ namespace Laboratorio_1_Juan_Roncagliolo
     {
         static void Main(string[] args)
         {
+            //Programa parte 1:
 
+            //Asignar e imprimir nombre del jugador 1:
+
+            Console.WriteLine("Parte 1: \n");
+            Clase_Persona persona1 = new Clase_Persona();
+            string Name_1 = persona1.Name = "Juan";
+            string LastName_1 = persona1.LastName = "Roncagliolo";
+            Console.WriteLine("Nombre jugador: " + Name_1 + " " + LastName_1+"\n");
+
+            // Asignar, convertir e imprimir jugada del jugador 1: 
+
+            Console.WriteLine("Presione cualquier tecla para hacer el lanzamiento: ");
+            Console.ReadKey();
+            int jugada_1 = persona1.Lanzar();
+            string conv_jugada_1;
+            if (jugada_1 == 0)
+            {
+                conv_jugada_1 = "Piedra";
+            }
+            else if (jugada_1 == 1)
+            {
+                conv_jugada_1 = "Papel";
+            }
+            else
+            {
+                conv_jugada_1 = "Tijera";
+            }
+            Console.WriteLine("Valor jugada: ("+jugada_1+") = "+ conv_jugada_1);
+            Console.ReadKey();
         }
     }
 }
