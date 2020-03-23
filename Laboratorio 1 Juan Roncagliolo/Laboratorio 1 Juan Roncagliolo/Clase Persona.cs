5ab9c52fc8f2b9ee850a43a9e03f5daa3fe91743
juan ignacio roncagliolo <jironcagliolo@miuandes.cl>
2020-03-23 18:34:42 -0300

    Parte 2 Incompleta (falta revisar)

--- Laboratorio 1 Juan Roncagliolo/Laboratorio 1 Juan Roncagliolo/Clase Persona.cs
+++ Laboratorio 1 Juan Roncagliolo/Laboratorio 1 Juan Roncagliolo/Clase Persona.cs
@@ -9,15 +9,26 @@ namespace Laboratorio_1_Juan_Roncagliolo
     class Clase_Persona
     {
         //Creacion de atributos
+
         public string Name;
         public string LastName;
+        public static string Name_1 = "Juan";
+        public static string LastName_1 = "Roncagliolo";
         //Creacion metodo lanzar
+
         public int Lanzar()
         {
             Random rnd = new Random();
             int hand = rnd.Next(0,3);
             return hand;
         }
-
+        //Parte 2:
+        //Creacion de un cosntructor:
+        //Un constructor es un metodo para la asignacion de atributos
+        public Clase_Persona(string n, string ln)
+        {
+            Name = n;
+            LastName = ln;
+        }
     }
 }
--- Laboratorio 1 Juan Roncagliolo/Laboratorio 1 Juan Roncagliolo/Program.cs
+++ Laboratorio 1 Juan Roncagliolo/Laboratorio 1 Juan Roncagliolo/Program.cs
@@ -12,34 +12,64 @@ namespace Laboratorio_1_Juan_Roncagliolo
         {
             //Programa parte 1:
 
-            //Asignar e imprimir nombre del jugador 1:
-
-            Console.WriteLine("Parte 1: \n");
-            Clase_Persona persona1 = new Clase_Persona();
-            string Name_1 = persona1.Name = "Juan";
-            string LastName_1 = persona1.LastName = "Roncagliolo";
-            Console.WriteLine("Nombre jugador: " + Name_1 + " " + LastName_1+"\n");
-
-            // Asignar, convertir e imprimir jugada del jugador 1: 
-
-            Console.WriteLine("Presione cualquier tecla para hacer el lanzamiento: ");
-            Console.ReadKey();
-            int jugada_1 = persona1.Lanzar();
-            string conv_jugada_1;
-            if (jugada_1 == 0)
+            //Imprimir nombre del jugador 1:
             {
-                conv_jugada_1 = "Piedra";
+                Console.WriteLine("------------Parte 1:------------\n");
+                Console.WriteLine("Nombre jugador: " + Clase_Persona.Name_1 + " " + Clase_Persona.LastName_1 + "\n");
+
+                // Asignar, convertir e imprimir jugada del jugador 1: 
+
+                Clase_Persona persona1 = new Clase_Persona("Juan", "Roncagliolo");
+                int jugada_1 = persona1.Lanzar();
+                string conv_jugada_1;
+                if (jugada_1 == 0)
+                {
+                    conv_jugada_1 = "Piedra";
+                }
+                else if (jugada_1 == 1)
+                {
+                    conv_jugada_1 = "Papel";
+                }
+                else
+                {
+                    conv_jugada_1 = "Tijera";
+                }
+                Console.WriteLine("Valor jugada: (" + jugada_1 + ") = " + conv_jugada_1);
+                Console.WriteLine("\n----------Fin parte 1-----------\n\nPresione 'Enter' para continuar: \n");
+                Console.ReadKey();
             }
-            else if (jugada_1 == 1)
+
+            //Parte 2:
+
+            //Utilizacion del contructor:
             {
-                conv_jugada_1 = "Papel";
+                Console.WriteLine("------------Parte 2:------------\n");
+                Console.WriteLine("Introduzca su nombre: ");
+                string Name = Console.ReadLine();
+                Console.WriteLine("\nIntroduzca su apellido: ");
+                string LastName = Console.ReadLine();
+                Clase_Persona persona2 = new Clase_Persona(Name, LastName);
+                Console.WriteLine("\nNombre jugador: " + persona2.Name + " " + persona2.LastName + "\n");
+
+                int jugada_2 = persona2.Lanzar();
+                string conv_jugada_2;
+                if (jugada_2 == 0)
+                {
+                    conv_jugada_2 = "Piedra";
+                }
+                else if (jugada_2 == 1)
+                {
+                    conv_jugada_2 = "Papel";
+                }
+                else
+                {
+                    conv_jugada_2 = "Tijera";
+                }
+                Console.WriteLine("Valor jugada: (" + jugada_2 + ") = " + conv_jugada_2);
+                Console.WriteLine("\n----------Fin parte 2-----------\n\nPresione 'Enter' para salir: \n");
+                Console.ReadKey();
             }
-            else
-            {
-                conv_jugada_1 = "Tijera";
-            }
-            Console.WriteLine("Valor jugada: ("+jugada_1+") = "+ conv_jugada_1);
-            Console.ReadKey();
+
         }
     }
 }
